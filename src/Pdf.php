907104db4688bc6702bf553253391966f55872c8
Elzo Brito dos Santos Filho <elzo.santos@cps.sp.gov.br>
2021-10-29 00:40:29 -0300

    inicial

---
+++ src/Pdf.php
@@ -0,0 +1,357 @@
+<?php
+
+namespace Oliviapdf;
+
+use Fpdf\Fpdf;
+
+class Pdf extends Fpdf
+{
+    var $B = 0;
+    var $I = 0;
+    var $U = 0;
+    var $HREF = '';
+    var $ALIGN = '';
+
+    var $widths;
+    var $aligns;
+
+    //Footer
+    var $autentication = null;
+    var $end_page = '/{nb}';
+
+    //Header
+    var $image_logo = null;
+    var $title = null;
+    var $title_config_array = null;
+    var $subtitle = null;
+    var $subtitle_config_array = null;
+
+    function Circle($x, $y, $r, $style = 'D')
+    {
+        $this->Ellipse($x, $y, $r, $r, $style);
+    }
+
+    function Ellipse($x, $y, $rx, $ry, $style = 'D')
+    {
+        if ($style == 'F')
+            $op = 'f';
+        elseif ($style == 'FD' || $style == 'DF')
+            $op = 'B';
+        else
+            $op = 'S';
+        $lx = 4 / 3 * (M_SQRT2 - 1) * $rx;
+        $ly = 4 / 3 * (M_SQRT2 - 1) * $ry;
+        $k = $this->k;
+        $h = $this->h;
+        $this->_out(sprintf(
+            '%.2F %.2F m %.2F %.2F %.2F %.2F %.2F %.2F c',
+            ($x + $rx) * $k,
+            ($h - $y) * $k,
+            ($x + $rx) * $k,
+            ($h - ($y - $ly)) * $k,
+            ($x + $lx) * $k,
+            ($h - ($y - $ry)) * $k,
+            $x * $k,
+            ($h - ($y - $ry)) * $k
+        ));
+        $this->_out(sprintf(
+            '%.2F %.2F %.2F %.2F %.2F %.2F c',
+            ($x - $lx) * $k,
+            ($h - ($y - $ry)) * $k,
+            ($x - $rx) * $k,
+            ($h - ($y - $ly)) * $k,
+            ($x - $rx) * $k,
+            ($h - $y) * $k
+        ));
+        $this->_out(sprintf(
+            '%.2F %.2F %.2F %.2F %.2F %.2F c',
+            ($x - $rx) * $k,
+            ($h - ($y + $ly)) * $k,
+            ($x - $lx) * $k,
+            ($h - ($y + $ry)) * $k,
+            $x * $k,
+            ($h - ($y + $ry)) * $k
+        ));
+        $this->_out(sprintf(
+            '%.2F %.2F %.2F %.2F %.2F %.2F c %s',
+            ($x + $lx) * $k,
+            ($h - ($y + $ry)) * $k,
+            ($x + $rx) * $k,
+            ($h - ($y + $ly)) * $k,
+            ($x + $rx) * $k,
+            ($h - $y) * $k,
+            $op
+        ));
+    }
+
+    function SetWidths($w)
+    {
+        //Set the array of column widths
+        $this->widths = $w;
+    }
+
+    function SetAligns($a)
+    {
+        //Set the array of column alignments
+        $this->aligns = $a;
+    }
+
+    function Row($data, $no_border = null)
+    {
+        //Calculate the height of the row
+        $nb = 0;
+
+        for ($i = 0; $i < count($data); $i++)
+            $nb = max($nb, $this->NbLines($this->widths[$i], $data[$i]));
+        $h = 5 * $nb;
+        //Issue a page break first if needed
+        $this->CheckPageBreak($h);
+        //Draw the cells of the row
+        // Color and font restoration
+        // $this->SetFillColor(224, 235, 255);
+        // $this->SetTextColor(0);
+        // $this->SetFont('');
+
+        $fill = false;
+        for ($i = 0; $i < count($data); $i++) {
+            $w = $this->widths[$i];
+            $a = isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
+            //Save the current position
+            $x = $this->GetX();
+            $y = $this->GetY();
+            //Draw the border
+            if (!$no_border)
+                $this->Rect($x, $y, $w, $h);
+
+            //Print the text
+            $this->MultiCell($w, 5, $data[$i], 0, $a, $fill);
+            //Put the position to the right of the cell
+            $this->SetXY($x + $w, $y);
+            //            $fill = !$fill;
+        }
+        //Go to the next line
+        $this->Ln($h);
+    }
+
+    function CheckPageBreak($h)
+    {
+        //If the height h would cause an overflow, add a new page immediately
+        if ($this->GetY() + $h > $this->PageBreakTrigger)
+            $this->AddPage($this->CurOrientation);
+    }
+
+    function NbLines($w, $txt)
+    {
+        //Computes the number of lines a MultiCell of width w will take
+        $cw = &$this->CurrentFont['cw'];
+        if ($w == 0)
+            $w = $this->w - $this->rMargin - $this->x;
+        $wmax = ($w - 2 * $this->cMargin) * 1000 / $this->FontSize;
+        $s = str_replace("\r", '', $txt);
+        $nb = strlen($s);
+        if ($nb > 0 and $s[$nb - 1] == "\n")
+            $nb--;
+        $sep = -1;
+        $i = 0;
+        $j = 0;
+        $l = 0;
+        $nl = 1;
+        while ($i < $nb) {
+            $c = $s[$i];
+            if ($c == "\n") {
+                $i++;
+                $sep = -1;
+                $j = $i;
+                $l = 0;
+                $nl++;
+                continue;
+            }
+            if ($c == ' ')
+                $sep = $i;
+            $l += $cw[$c];
+            if ($l > $wmax) {
+                if ($sep == -1) {
+                    if ($i == $j)
+                        $i++;
+                } else
+                    $i = $sep + 1;
+                $sep = -1;
+                $j = $i;
+                $l = 0;
+                $nl++;
+            } else
+                $i++;
+        }
+        return $nl;
+    }
+
+    function WriteHTML($html)
+    {
+        //HTML parser
+        $html = str_replace("\n", ' ', $html);
+        $a = preg_split('/<(.*)>/U', $html, -1, PREG_SPLIT_DELIM_CAPTURE);
+        foreach ($a as $i => $e) {
+            if ($i % 2 == 0) {
+                //Text
+                if ($this->HREF)
+                    $this->PutLink($this->HREF, $e);
+                elseif ($this->ALIGN == 'center')
+                    $this->Cell(0, 5, $e, 0, 1, 'C');
+                else
+                    $this->Write(5, $e);
+            } else {
+                //Tag
+                if ($e[0] == '/')
+                    $this->CloseTag(strtoupper(substr($e, 1)));
+                else {
+                    //Extract properties
+                    $a2 = explode(' ', $e);
+                    $tag = strtoupper(array_shift($a2));
+                    $prop = array();
+                    foreach ($a2 as $v) {
+                        if (preg_match('/([^=]*)=["\']?([^"\']*)/', $v, $a3))
+                            $prop[strtoupper($a3[1])] = $a3[2];
+                    }
+                    $this->OpenTag($tag, $prop);
+                }
+            }
+        }
+    }
+
+    function OpenTag($tag, $prop)
+    {
+        //Opening tag
+        if ($tag == 'B' || $tag == 'I' || $tag == 'U')
+            $this->SetStyle($tag, true);
+        if ($tag == 'A')
+            $this->HREF = $prop['HREF'];
+        if ($tag == 'BR')
+            $this->Ln(5);
+        if ($tag == 'P')
+            $this->ALIGN = $prop['ALIGN'];
+        if ($tag == 'HR') {
+            if (!empty($prop['WIDTH']))
+                $Width = $prop['WIDTH'];
+            else
+                $Width = $this->w - $this->lMargin - $this->rMargin;
+            $this->Ln(2);
+            $x = $this->GetX();
+            $y = $this->GetY();
+            $this->SetLineWidth(0.4);
+            $this->Line($x, $y, $x + $Width, $y);
+            $this->SetLineWidth(0.2);
+            $this->Ln(2);
+        }
+    }
+
+    function CloseTag($tag)
+    {
+        //Closing tag
+        if ($tag == 'B' || $tag == 'I' || $tag == 'U')
+            $this->SetStyle($tag, false);
+        if ($tag == 'A')
+            $this->HREF = '';
+        if ($tag == 'P')
+            $this->ALIGN = '';
+    }
+
+    function SetStyle($tag, $enable)
+    {
+        //Modify style and select corresponding font
+        $this->$tag += ($enable ? 1 : -1);
+        $style = '';
+        foreach (array('B', 'I', 'U') as $s)
+            if ($this->$s > 0)
+                $style .= $s;
+        $this->SetFont('', $style);
+    }
+
+    function PutLink($URL, $txt)
+    {
+        //Put a hyperlink
+        $this->SetTextColor(0, 0, 255);
+        $this->SetStyle('U', true);
+        $this->Write(5, $txt, $URL);
+        $this->SetStyle('U', false);
+        $this->SetTextColor(0);
+    }
+
+
+    function TextWithDirection($x, $y, $txt, $direction = 'R')
+    {
+        if ($direction == 'R')
+            $s = sprintf('BT %.2F %.2F %.2F %.2F %.2F %.2F Tm (%s) Tj ET', 1, 0, 0, 1, $x * $this->k, ($this->h - $y) * $this->k, $this->_escape($txt));
+        elseif ($direction == 'L')
+            $s = sprintf('BT %.2F %.2F %.2F %.2F %.2F %.2F Tm (%s) Tj ET', -1, 0, 0, -1, $x * $this->k, ($this->h - $y) * $this->k, $this->_escape($txt));
+        elseif ($direction == 'U')
+            $s = sprintf('BT %.2F %.2F %.2F %.2F %.2F %.2F Tm (%s) Tj ET', 0, 1, -1, 0, $x * $this->k, ($this->h - $y) * $this->k, $this->_escape($txt));
+        elseif ($direction == 'D')
+            $s = sprintf('BT %.2F %.2F %.2F %.2F %.2F %.2F Tm (%s) Tj ET', 0, -1, 1, 0, $x * $this->k, ($this->h - $y) * $this->k, $this->_escape($txt));
+        else
+            $s = sprintf('BT %.2F %.2F Td (%s) Tj ET', $x * $this->k, ($this->h - $y) * $this->k, $this->_escape($txt));
+        if ($this->ColorFlag)
+            $s = 'q ' . $this->TextColor . ' ' . $s . ' Q';
+        $this->_out($s);
+    }
+
+    function Header()
+    {
+        if ($this->image_logo)
+            // Logo
+            $this->Image($this->image_logo, 75, 10, 50);
+        // Arial bold 15
+        $this->Rect(40, 35, 120, 0, 1);
+        $this->SetTextColor(110, 19, 28);
+
+        $this->SetFont('Arial', 'B', 10);
+        if ($this->title_config_array != null)
+            $this->SetFont($this->title_config_array[0], $this->title_config_array[1], $this->title_config_array[2]);
+            
+        $this->SetXY(10, 37);
+        if ($this->title)
+            $this->MultiCell(190, 7, iconv('utf-8', 'cp1252', $this->title), 0, 'C', 0);
+
+        if ($this->subtitle_config_array != null)
+            $this->SetFont($this->subtitle_config_array[0], $this->subtitle_config_array[1], $this->subtitle_config_array[2]);
+        if ($this->subtitle)
+            $this->MultiCell(190, 7, iconv('utf-8', 'cp1252', $this->subtitle), 0, 'C', 0);
+        // Line break
+        $this->Ln(5);
+    }
+
+    function Footer()
+    {
+        // Position at 1.5 cm from bottom
+        $this->SetY(-15);
+        // Arial italic 8
+        $this->SetFont('Arial', 'I', 8);
+        // Page number
+        $this->Cell(0, 10, iconv('utf-8', 'cp1252', 'Página ') . $this->PageNo() . '/{nb}', 0, 0, 'C');
+
+        //$this->end_page = $this->PageNo() . '/{nb}';
+
+        if ($this->autentication != null) {
+            $this->SetFont('Arial', '', 8);
+            $this->SetXY(10, 10);
+            $this->TextWithRotation(205, 280, iconv('utf-8', 'cp1252', 'Autenticação:  ') . $this->autentication, 90, 0);
+        }
+    }
+
+
+    function TextWithRotation($x, $y, $txt, $txt_angle, $font_angle = 0)
+    {
+        $font_angle += 90 + $txt_angle;
+        $txt_angle *= M_PI / 180;
+        $font_angle *= M_PI / 180;
+
+        $txt_dx = cos($txt_angle);
+        $txt_dy = sin($txt_angle);
+        $font_dx = cos($font_angle);
+        $font_dy = sin($font_angle);
+
+        $s = sprintf('BT %.2F %.2F %.2F %.2F %.2F %.2F Tm (%s) Tj ET', $txt_dx, $txt_dy, $font_dx, $font_dy, $x * $this->k, ($this->h - $y) * $this->k, $this->_escape($txt));
+        if ($this->ColorFlag)
+            $s = 'q ' . $this->TextColor . ' ' . $s . ' Q';
+        $this->_out($s);
+    }
+}
